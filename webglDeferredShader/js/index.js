657718292f446b734a7c1d0c26cbf354a6fccc7f
YuqinShao <winifred-shao@hotmail.com>
2013-11-23 19:50:50 -0500

    add fps

--- webglDeferredShader/js/index.js
+++ webglDeferredShader/js/index.js
@@ -1,5 +1,8 @@
 (function(){
 	
+    var stats = new Stats();
+   
+
 	var gl;
 	var view,presp,model,inverse;
     var checkboxs =[]; // 0:normal,1:color,2:position,3: depth, 4,diffuse
@@ -332,221 +335,221 @@
 	function initMeshBuffers()
 	{
         //cube
-        meshvbo = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER,meshvbo);
-        vertices = [
-            // Front face
-            -1.0, -1.0,  1.0,
-             1.0, -1.0,  1.0,
-             1.0,  1.0,  1.0,
-            -1.0,  1.0,  1.0,
+        // meshvbo = gl.createBuffer();
+        // gl.bindBuffer(gl.ARRAY_BUFFER,meshvbo);
+        // vertices = [
+        //     // Front face
+        //     -1.0, -1.0,  1.0,
+        //      1.0, -1.0,  1.0,
+        //      1.0,  1.0,  1.0,
+        //     -1.0,  1.0,  1.0,
 
-            // Back face
-            -1.0, -1.0, -1.0,
-            -1.0,  1.0, -1.0,
-             1.0,  1.0, -1.0,
-             1.0, -1.0, -1.0,
+        //     // Back face
+        //     -1.0, -1.0, -1.0,
+        //     -1.0,  1.0, -1.0,
+        //      1.0,  1.0, -1.0,
+        //      1.0, -1.0, -1.0,
 
-            // Top face
-            -1.0,  1.0, -1.0,
-            -1.0,  1.0,  1.0,
-             1.0,  1.0,  1.0,
-             1.0,  1.0, -1.0,
+        //     // Top face
+        //     -1.0,  1.0, -1.0,
+        //     -1.0,  1.0,  1.0,
+        //      1.0,  1.0,  1.0,
+        //      1.0,  1.0, -1.0,
 
-            // Bottom face
-            -1.0, -1.0, -1.0,
-             1.0, -1.0, -1.0,
-             1.0, -1.0,  1.0,
-            -1.0, -1.0,  1.0,
+        //     // Bottom face
+        //     -1.0, -1.0, -1.0,
+        //      1.0, -1.0, -1.0,
+        //      1.0, -1.0,  1.0,
+        //     -1.0, -1.0,  1.0,
 
-            // Right face
-             1.0, -1.0, -1.0,
-             1.0,  1.0, -1.0,
-             1.0,  1.0,  1.0,
-             1.0, -1.0,  1.0,
+        //     // Right face
+        //      1.0, -1.0, -1.0,
+        //      1.0,  1.0, -1.0,
+        //      1.0,  1.0,  1.0,
+        //      1.0, -1.0,  1.0,
 
-            // Left face
-            -1.0, -1.0, -1.0,
-            -1.0, -1.0,  1.0,
-            -1.0,  1.0,  1.0,
-            -1.0,  1.0, -1.0,
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        meshvbo.itemSize = 3;
-        meshvbo.numItems = 24;
+        //     // Left face
+        //     -1.0, -1.0, -1.0,
+        //     -1.0, -1.0,  1.0,
+        //     -1.0,  1.0,  1.0,
+        //     -1.0,  1.0, -1.0,
+        // ];
+        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+        // meshvbo.itemSize = 3;
+        // meshvbo.numItems = 24;
+
+        // meshnbo = gl.createBuffer();
+        // gl.bindBuffer(gl.ARRAY_BUFFER, meshnbo);
+        // var vertexNormals = [
+        //     // Front face
+        //      0.0,  0.0,  1.0,
+        //      0.0,  0.0,  1.0,
+        //      0.0,  0.0,  1.0,
+        //      0.0,  0.0,  1.0,
+
+        //     // Back face
+        //      0.0,  0.0, -1.0,
+        //      0.0,  0.0, -1.0,
+        //      0.0,  0.0, -1.0,
+        //      0.0,  0.0, -1.0,
+
+        //     // Top face
+        //      0.0,  1.0,  0.0,
+        //      0.0,  1.0,  0.0,
+        //      0.0,  1.0,  0.0,
+        //      0.0,  1.0,  0.0,
+
+        //     // Bottom face
+        //      0.0, -1.0,  0.0,
+        //      0.0, -1.0,  0.0,
+        //      0.0, -1.0,  0.0,
+        //      0.0, -1.0,  0.0,
+
+        //     // Right face
+        //      1.0,  0.0,  0.0,
+        //      1.0,  0.0,  0.0,
+        //      1.0,  0.0,  0.0,
+        //      1.0,  0.0,  0.0,
+
+        //     // Left face
+        //     -1.0,  0.0,  0.0,
+        //     -1.0,  0.0,  0.0,
+        //     -1.0,  0.0,  0.0,
+        //     -1.0,  0.0,  0.0,
+        // ];
+        //  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
+        //  meshnbo.itemSize = 3;
+        //  meshnbo.numItems = 24;
+
+        //  meshtbo = gl.createBuffer();
+        //  gl.bindBuffer(gl.ARRAY_BUFFER, meshtbo);
+        //  var textureCoords = [
+        //     // Front face
+        //     0.0, 0.0,
+        //     1.0, 0.0,
+        //     1.0, 1.0,
+        //     0.0, 1.0,
+
+        //     // Back face
+        //     1.0, 0.0,
+        //     1.0, 1.0,
+        //     0.0, 1.0,
+        //     0.0, 0.0,
+
+        //     // Top face
+        //     0.0, 1.0,
+        //     0.0, 0.0,
+        //     1.0, 0.0,
+        //     1.0, 1.0,
+
+        //     // Bottom face
+        //     1.0, 1.0,
+        //     0.0, 1.0,
+        //     0.0, 0.0,
+        //     1.0, 0.0,
+
+        //     // Right face
+        //     1.0, 0.0,
+        //     1.0, 1.0,
+        //     0.0, 1.0,
+        //     0.0, 0.0,
+
+        //     // Left face
+        //     0.0, 0.0,
+        //     1.0, 0.0,
+        //     1.0, 1.0,
+        //     0.0, 1.0
+        // ];
+        // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+        // meshtbo.itemSize = 2;
+        // meshtbo.numItems = 24;
+
+        // meshibo = gl.createBuffer();
+        // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshibo);
+        // var cubeVertexIndices = [
+        //     0, 1, 2,      0, 2, 3,    // Front face
+        //     4, 5, 6,      4, 6, 7,    // Back face
+        //     8, 9, 10,     8, 10, 11,  // Top face
+        //     12, 13, 14,   12, 14, 15, // Bottom face
+        //     16, 17, 18,   16, 18, 19, // Right face
+        //     20, 21, 22,   20, 22, 23  // Left face
+        // ];
+        // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+        // meshibo.itemSize = 1;
+        // meshibo.numItems = 36;
+
+		var latitudeBands = 30;
+        var longitudeBands = 30;
+        var radius = 1.0;
+
+        var vertexPositionData = [];
+        var normalData = [];
+        var textureCoordData = [];
+        for (var latNumber=0; latNumber <= latitudeBands; latNumber++) {
+            var theta = latNumber * Math.PI / latitudeBands;
+            var sinTheta = Math.sin(theta);
+            var cosTheta = Math.cos(theta);
+
+            for (var longNumber=0; longNumber <= longitudeBands; longNumber++) {
+                var phi = longNumber * 2 * Math.PI / longitudeBands;
+                var sinPhi = Math.sin(phi);
+                var cosPhi = Math.cos(phi);
+
+                var x = cosPhi * sinTheta;
+                var y = cosTheta;
+                var z = sinPhi * sinTheta;
+                var u = 1 - (longNumber / longitudeBands);
+                var v = 1 - (latNumber / latitudeBands);
+
+                normalData.push(x);
+                normalData.push(y);
+                normalData.push(z);
+                textureCoordData.push(u);
+                textureCoordData.push(v);
+                vertexPositionData.push(radius * x);
+                vertexPositionData.push(radius * y);
+                vertexPositionData.push(radius * z);
+            }
+        }
+
+        var indexData = [];
+        for (var latNumber=0; latNumber < latitudeBands; latNumber++) {
+            for (var longNumber=0; longNumber < longitudeBands; longNumber++) {
+                var first = (latNumber * (longitudeBands + 1)) + longNumber;
+                var second = first + longitudeBands + 1;
+                indexData.push(first);
+                indexData.push(second);
+                indexData.push(first + 1);
+
+                indexData.push(second);
+                indexData.push(second + 1);
+                indexData.push(first + 1);
+            }
+        }
 
         meshnbo = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, meshnbo);
-        var vertexNormals = [
-            // Front face
-             0.0,  0.0,  1.0,
-             0.0,  0.0,  1.0,
-             0.0,  0.0,  1.0,
-             0.0,  0.0,  1.0,
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalData), gl.STATIC_DRAW);
+        meshnbo.itemSize = 3;
+        meshnbo.numItems = normalData.length / 3;
 
-            // Back face
-             0.0,  0.0, -1.0,
-             0.0,  0.0, -1.0,
-             0.0,  0.0, -1.0,
-             0.0,  0.0, -1.0,
-
-            // Top face
-             0.0,  1.0,  0.0,
-             0.0,  1.0,  0.0,
-             0.0,  1.0,  0.0,
-             0.0,  1.0,  0.0,
-
-            // Bottom face
-             0.0, -1.0,  0.0,
-             0.0, -1.0,  0.0,
-             0.0, -1.0,  0.0,
-             0.0, -1.0,  0.0,
-
-            // Right face
-             1.0,  0.0,  0.0,
-             1.0,  0.0,  0.0,
-             1.0,  0.0,  0.0,
-             1.0,  0.0,  0.0,
-
-            // Left face
-            -1.0,  0.0,  0.0,
-            -1.0,  0.0,  0.0,
-            -1.0,  0.0,  0.0,
-            -1.0,  0.0,  0.0,
-        ];
-         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
-         meshnbo.itemSize = 3;
-         meshnbo.numItems = 24;
-
-         meshtbo = gl.createBuffer();
-         gl.bindBuffer(gl.ARRAY_BUFFER, meshtbo);
-         var textureCoords = [
-            // Front face
-            0.0, 0.0,
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0,
-
-            // Back face
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0,
-            0.0, 0.0,
-
-            // Top face
-            0.0, 1.0,
-            0.0, 0.0,
-            1.0, 0.0,
-            1.0, 1.0,
-
-            // Bottom face
-            1.0, 1.0,
-            0.0, 1.0,
-            0.0, 0.0,
-            1.0, 0.0,
-
-            // Right face
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0,
-            0.0, 0.0,
-
-            // Left face
-            0.0, 0.0,
-            1.0, 0.0,
-            1.0, 1.0,
-            0.0, 1.0
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+        meshtbo = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, meshtbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
         meshtbo.itemSize = 2;
-        meshtbo.numItems = 24;
+        meshtbo.numItems = textureCoordData.length / 2;
+
+        meshvbo = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, meshvbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositionData), gl.STATIC_DRAW);
+        meshvbo.itemSize = 3;
+        meshvbo.numItems = vertexPositionData.length / 3;
 
         meshibo = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshibo);
-        var cubeVertexIndices = [
-            0, 1, 2,      0, 2, 3,    // Front face
-            4, 5, 6,      4, 6, 7,    // Back face
-            8, 9, 10,     8, 10, 11,  // Top face
-            12, 13, 14,   12, 14, 15, // Bottom face
-            16, 17, 18,   16, 18, 19, // Right face
-            20, 21, 22,   20, 22, 23  // Left face
-        ];
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
         meshibo.itemSize = 1;
-        meshibo.numItems = 36;
-
-		// var latitudeBands = 30;
-  //       var longitudeBands = 30;
-  //       var radius = 1.0;
-
-  //       var vertexPositionData = [];
-  //       var normalData = [];
-  //       var textureCoordData = [];
-  //       for (var latNumber=0; latNumber <= latitudeBands; latNumber++) {
-  //           var theta = latNumber * Math.PI / latitudeBands;
-  //           var sinTheta = Math.sin(theta);
-  //           var cosTheta = Math.cos(theta);
-
-  //           for (var longNumber=0; longNumber <= longitudeBands; longNumber++) {
-  //               var phi = longNumber * 2 * Math.PI / longitudeBands;
-  //               var sinPhi = Math.sin(phi);
-  //               var cosPhi = Math.cos(phi);
-
-  //               var x = cosPhi * sinTheta;
-  //               var y = cosTheta;
-  //               var z = sinPhi * sinTheta;
-  //               var u = 1 - (longNumber / longitudeBands);
-  //               var v = 1 - (latNumber / latitudeBands);
-
-  //               normalData.push(x);
-  //               normalData.push(y);
-  //               normalData.push(z);
-  //               textureCoordData.push(u);
-  //               textureCoordData.push(v);
-  //               vertexPositionData.push(radius * x);
-  //               vertexPositionData.push(radius * y);
-  //               vertexPositionData.push(radius * z);
-  //           }
-  //       }
-
-  //       var indexData = [];
-  //       for (var latNumber=0; latNumber < latitudeBands; latNumber++) {
-  //           for (var longNumber=0; longNumber < longitudeBands; longNumber++) {
-  //               var first = (latNumber * (longitudeBands + 1)) + longNumber;
-  //               var second = first + longitudeBands + 1;
-  //               indexData.push(first);
-  //               indexData.push(second);
-  //               indexData.push(first + 1);
-
-  //               indexData.push(second);
-  //               indexData.push(second + 1);
-  //               indexData.push(first + 1);
-  //           }
-  //       }
-
-  //       meshnbo = gl.createBuffer();
-  //       gl.bindBuffer(gl.ARRAY_BUFFER, meshnbo);
-  //       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalData), gl.STATIC_DRAW);
-  //       meshnbo.itemSize = 3;
-  //       meshnbo.numItems = normalData.length / 3;
-
-  //       meshtbo = gl.createBuffer();
-  //       gl.bindBuffer(gl.ARRAY_BUFFER, meshtbo);
-  //       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordData), gl.STATIC_DRAW);
-  //       meshtbo.itemSize = 2;
-  //       meshtbo.numItems = textureCoordData.length / 2;
-
-  //       meshvbo = gl.createBuffer();
-  //       gl.bindBuffer(gl.ARRAY_BUFFER, meshvbo);
-  //       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexPositionData), gl.STATIC_DRAW);
-  //       meshvbo.itemSize = 3;
-  //       meshvbo.numItems = vertexPositionData.length / 3;
-
-  //       meshibo = gl.createBuffer();
-  //       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshibo);
-  //       gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexData), gl.STATIC_DRAW);
-  //       meshibo.itemSize = 1;
-  //       meshibo.numItems = indexData.length;
+        meshibo.numItems = indexData.length;
 	}
     var time = 0;
 
@@ -597,7 +600,7 @@
         mat4.perspective(45.0,gl.viewportWidth/gl.viewportHeight,0.1,100.0,persp);
 
         mat4.identity(model);
-        mat4.scale(model,[2.5,0.5,0.5]);
+        mat4.scale(model,[0.5,0.5,0.5]);
 
         var mv = mat4.create();
         mat4.multiply(view, model, mv);
@@ -710,13 +713,28 @@
         lastMouseY = newY;
     }
 
-    
+    canvas.onmousedown = handleMouseDown;
+    canvas.oncontextmenu = function(ev) {return false;};
+    document.onmouseup = handleMouseUp;
+    document.onmousemove = handleMouseMove;
+    stats.setMode(0); // 0: fps, 1: ms
+
+    // Align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '100px';
+
+    document.body.appendChild( stats.domElement );    
+
  	function tick() {
         requestAnimFrame(tick);
-        drawScene();
-        time ++;
-        //console.log(time);
+        drawScene();    
+        
+        stats.update();
+        //time ++;
+        
     }
+    
 
     function initCheckbox(){
 
@@ -746,10 +764,12 @@
         //         checkboxs[i].checked = true;
         //     else
         //         checkboxs[i].checked = false;
-
         // }
-    }
 
+    }
+ 
+    
+   
 	(function loadWebGL(){        
 		var canvas = document.getElementById("canvas");
 		initGL(canvas);
@@ -760,15 +780,9 @@
         initCheckbox()
 
 		initTextureFramebuffer();
-
 		gl.clearColor(0.0,0.0,0,1.0);
 		gl.enable(gl.DEPTH_TEST);
-		
-		canvas.onmousedown = handleMouseDown;
-	    canvas.oncontextmenu = function(ev) {return false;};
-	    document.onmouseup = handleMouseUp;
-	    document.onmousemove = handleMouseMove;
 
-	    tick();
+        tick();	    
 	})();
 })();
